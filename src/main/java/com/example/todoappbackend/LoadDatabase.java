69ba5180ef1295efd609dabaa4b8417d4c6f9f64
ibnISSA <doudou.ndiaye663@gmail.com>
2024-05-17 10:41:04 +0000

    Changing LoadDatabase Category Enum

--- src/main/java/com/example/todoappbackend/LoadDatabase.java
+++ src/main/java/com/example/todoappbackend/LoadDatabase.java
@@ -27,15 +27,15 @@ class LoadDatabase {
             adminUser.setPassword("admin");
             log.info("Preloading " + userRepository.save(adminUser));
 
-            log.info("Preloading " + taskRepository.save(new Task("Daily meeting with team", new Date().toString(), false, Category.business)));
-            log.info("Preloading " + taskRepository.save(new Task("Pay for rent", new Date().toString(), true, Category.personal)));
-            log.info("Preloading " + taskRepository.save(new Task("Check emails", new Date().toString(), false, Category.personal)));
-            log.info("Preloading " + taskRepository.save(new Task("Laundry", new Date().toString(), false, Category.personal)));
-            log.info("Preloading " + taskRepository.save(new Task("Lunch with client", new Date().toString(), false, Category.business)));
-            log.info("Preloading " + taskRepository.save(new Task("New project kick off prep", new Date().toString(), false, Category.business)));
-            log.info("Preloading " + taskRepository.save(new Task("Pick up fuel", new Date().toString(), false, Category.business)));
-            log.info("Preloading " + taskRepository.save(new Task("Quick run", new Date().toString(), false, Category.personal)));
-            log.info("Preloading " + taskRepository.save(new Task("CI/CD talk prep", new Date().toString(), false, Category.business)));
+            log.info("Preloading " + taskRepository.save(new Task("Daily meeting with team", new Date().toString(), false, Category.Business)));
+            log.info("Preloading " + taskRepository.save(new Task("Pay for rent", new Date().toString(), true, Category.Personal)));
+            log.info("Preloading " + taskRepository.save(new Task("Check emails", new Date().toString(), false, Category.Personal)));
+            log.info("Preloading " + taskRepository.save(new Task("Laundry", new Date().toString(), false, Category.Personal)));
+            log.info("Preloading " + taskRepository.save(new Task("Lunch with client", new Date().toString(), false, Category.Business)));
+            log.info("Preloading " + taskRepository.save(new Task("New project kick off prep", new Date().toString(), false, Category.Business)));
+            log.info("Preloading " + taskRepository.save(new Task("Pick up fuel", new Date().toString(), false, Category.Business)));
+            log.info("Preloading " + taskRepository.save(new Task("Quick run", new Date().toString(), false, Category.Personal)));
+            log.info("Preloading " + taskRepository.save(new Task("CI/CD talk prep", new Date().toString(), false, Category.Business)));
         };
     }
 }
